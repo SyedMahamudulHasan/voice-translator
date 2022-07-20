a2981ca7e48c79cc26b769e14ee5a70b007260ba
omedia23 <syedhasan.cse@gmail.com>
2022-07-20 11:51:36 +0600

    add homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:edvive_task1_syed_mahamudul_hasan/screens/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Translator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home:
+      home: const HomeScreen(),
     );
   }
 }
