6186f670edbb6c36e2c68bf818744d34e80758a8
omedia23 <syedhasan.cse@gmail.com>
2022-07-20 13:19:04 +0600

    navigate wih bottom navigator

--- lib/screens/dashScreen.dart
+++ lib/screens/dashScreen.dart
@@ -1,9 +1,7 @@
-import 'package:edvive_task1_syed_mahamudul_hasan/widgets/botton_nav_bar.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:edvive_task1_syed_mahamudul_hasan/screens/translatorScreen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
-import '../widgets/choose_language.dart';
+import 'homeScreem.dart';
 
 class DashScreen extends StatefulWidget {
   const DashScreen({Key? key}) : super(key: key);
@@ -13,13 +11,39 @@ class DashScreen extends StatefulWidget {
 }
 
 class _DashScreenState extends State<DashScreen> {
+  int _page = 0;
+  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  final List<Widget> screens = const [
+    HomeScreen(),
+    TranslatorScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: const BottonNavBar(),
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          items: const [
+            Icon(
+              Icons.home_outlined,
+              size: 30,
+            ),
+            Icon(
+              Icons.record_voice_over_sharp,
+              size: 30,
+            ),
+          ],
+          onTap: (index) {
+            // final navBarState = _bottomNavigationKey.currentState;
+            // navBarState?.setPage(index);
+            setState(() {
+              _page = index;
+            });
+          },
+        ),
         appBar: AppBar(
           title: const Text("Flutter Translator"),
         ),
-        body: Column());
+        body: screens[_page]);
   }
 }
